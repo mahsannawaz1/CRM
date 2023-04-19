633fc0ffa6d030431cd10f27a27ee0e49766d560
Ahsan Nawaz <nawazehsen@gmail.com>
2023-04-20 00:29:19 +0500

    Clear bugs in User authentication System

--- CRM/settings.py
+++ CRM/settings.py
@@ -119,13 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as log_views
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', views.home, name='dashboard'),
     path('register/', views.userRegister, name='register'),
-    path('login/', log_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('login', log_views.LoginView.as_view(
+        template_name='user/login.html'), name='login'),
     path('logout/', log_views.LogoutView.as_view(template_name='user/logout.html'), name='logout')
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
